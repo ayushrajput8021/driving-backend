ab804ef88712eace54510da311164040708fdf9b
Ayush <rajput.ayush8jn@gmail.com>
2024-02-08 00:21:53 +0530

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import userRouter from './routes/userRoutes';
 import testRouter from './routes/testRoutes';
 
 const app = express();
 
 //middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV?.trim() === 'dev') {
